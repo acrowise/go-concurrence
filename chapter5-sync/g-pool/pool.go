8b22ccc832de413202652208ba471627b80a4958
sam <sam@163.com>
2020-04-17 13:48:09 +0800

    pool

--- chapter5-sync/g-pool/pool.go
+++ chapter5-sync/g-pool/pool.go
@@ -7,7 +7,10 @@ import (
 	"sync"
 	"sync/atomic"
 )
-
+//临时对象池的第二个突出特性是对垃圾回收友好。
+//垃圾回收的执行一般会使临时对象池中的对象值被全部移除。
+//也就是说，即使我们永远不会显式的从临时对象池取走某一个对象值，该对象值也不会永远待在临时对象池中。它的生命周期取决于垃圾回收任务下一次的执行时间。
+//在这里，我们使用runtime/debug代码包的SetGCPercent函数来禁用、恢复GC以及指定垃圾收集比率，以保证我们的演示能够如愿进行。
 func main() {
 	// 禁用GC，并保证在main函数执行结束前恢复GC。
 	defer debug.SetGCPercent(debug.SetGCPercent(-1))
