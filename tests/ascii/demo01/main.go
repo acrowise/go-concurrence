0f7044ac4c9decc4c838838fb7df38a7e31f245b
sam <sam@163.com>
2020-04-28 08:11:51 +0800

    tst

---
+++ tests/ascii/demo01/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	e:="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	for i := 0; i < len(e); i++ {
+		ascii,_:=strconv.Atoi(fmt.Sprintf("%d",e[i]))
+		fmt.Printf("%c的ascii码值为%d\r\n",e[i],ascii)
+	}
+}
