fbe14bf88a58d3a9325c86f48842c4313d158295
sam <sam@163.com>
2020-05-13 08:16:56 +0800

    deadlock

--- chapter4-goroutine/b-deadlock/demo01/main.go
+++ chapter4-goroutine/b-deadlock/demo01/main.go
@@ -9,17 +9,17 @@ import (
 //这种所有线程或者进程(应该指的指的是除了g0之外的所有g吧)都在等待资源释放的情况，我们便把它称之为死锁。
 //fatal error: all goroutines are asleep - deadlock!
 //死锁是一个非常有意思的话题，常见的死锁大致分为以下几类：
-//i. 只在单一goroutine里操作信道，例子如上。
-//ii. 串联信道中间一环挂起
+//i. 只在单一goroutine里操作通道，例子如上。
+//ii. 串联通道中间一环挂起
 
 
-
+//todo  deadlock的理解见go-advanced
 func main() {
 
 
 	var  strChan chan  string
 	strChan = make(chan string,3)
-
+    //接收
 	elem:=<-strChan
 
 	fmt.Println(elem,"hello")
