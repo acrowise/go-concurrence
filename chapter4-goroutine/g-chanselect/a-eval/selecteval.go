df59ba87511ac762c4ebd1f3272df78b437a8c76
sam <sam@163.com>
2020-05-13 14:31:47 +0800

    selecteval

--- chapter4-goroutine/g-chanselect/a-eval/selecteval.go
+++ chapter4-goroutine/g-chanselect/a-eval/selecteval.go
@@ -1,14 +1,6 @@
 package main
 
 import "fmt"
-
-var intChan1 chan int
-var intChan2 chan int
-var channels = []chan int{intChan1, intChan2}
-
-var numbers = []int{1, 2, 3, 4, 5}
-
-
 //分支选择规则
 //(1)在开始执行select语句的时候，所有跟在case关键字右边的发送语句或接收语句中的通道表达式和元素表达式都会先求值
   //(求值的顺序是从左到右、自上而下的)，无论它们所在的case是否有可能被选择都会是这样
@@ -19,6 +11,12 @@ var numbers = []int{1, 2, 3, 4, 5}
    //这个判断还需要依据通道的具体特性(缓冲或非缓冲)以及那一时刻的具体情况来进行。只要发现有个case上的判断是肯定的，该case就会被选中
 //(4)当有一个case被选中时，运行时系统就会执行该case及其包含的语句，而其他case会被忽略。
    //如果同时有多个case满足条件，那么运行时系统会通过一个伪随机的算法选中一个case。
+
+var intChan1 chan int
+var intChan2 chan int
+var channels = []chan int{intChan1, intChan2}
+var numbers = []int{1, 2, 3, 4, 5}
+
 func main() {
 	select {
 	case getChan(0) <- getNumber(0): //把切片numbers中的第0个元素发送到切片channels中的第0个元素中
