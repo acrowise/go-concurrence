bdebcdddb4648677a63718bd32adcbf3f37b86e8
sam <sam@163.com>
2020-05-13 08:55:00 +0800

    chanval1

--- chapter4-goroutine/d-chanval/v1/chanval1.go
+++ chapter4-goroutine/d-chanval/v1/chanval1.go
@@ -6,10 +6,11 @@ import (
 )
 
 var mapChan = make(chan map[string]int, 1)
-
+//todo 演示通道元素值的传递的是副本还是什么，是否会相互影响
 func main() {
 	syncChan := make(chan struct{}, 2)
-	go func() { // 用于演示接收操作。
+	// 用于演示接收操作------------------------
+	go func() {
 		for {
 			if elem, ok := <-mapChan; ok {
 				elem["count"]++
@@ -20,21 +21,22 @@ func main() {
 		fmt.Println("Stopped. [receiver]")
 		syncChan <- struct{}{}
 	}()
-
-
-
-	//对countMap中的值的修改却是在接收方，但是由于map是引用类型，所以在发送方打印的时候就看到了端倪
-	go func() { // 用于演示发送操作。
+	// 用于演示发送操作-------------------
+	go func() {
 		countMap := make(map[string]int)
 		for i := 0; i < 5; i++ {
 			mapChan <- countMap
 			//之所以等一会打印，是想让接收方先把做修改，看看是否会影响发送方中的元素
 			time.Sleep(time.Millisecond)
+			//todo 对countMap中的值的修改却是在接收方，但是由于map是引用类型，所以在发送方打印的时候就看到了端倪
 			fmt.Printf("The count map: %v. [sender]\n", countMap)
 		}
 		close(mapChan)
 		syncChan <- struct{}{}
 	}()
+
+
+
 	<-syncChan
 	<-syncChan
 }
